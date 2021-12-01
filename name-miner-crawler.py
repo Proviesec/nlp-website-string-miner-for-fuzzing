e9c049c5c75069590a7b74f7e594c83603133ec3
Oliver Trabhardt <oliver.trabhardt@googlemail.com>
2021-12-01 19:30:21 +0100

    Update name-miner-crawler.py

--- name-miner-crawler.py
+++ name-miner-crawler.py
@@ -1,12 +1,32 @@
-import re
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('https://github.com')
-
-if r.status_code == 200:
-	soup: bs4.BeautifulSoup = BeautifulSoup(r.content, "html.parser")
-    
-    for headline in soup.find_all("span", {"class": "mw-headline"}):
-        print(headline.text)
+def crawl_miner(url,output,deep):
         
+    r = requests.get(url)
+
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.content, "html.parser")
+
+        for headline in soup.find_all("h1"):
+            for item in headline.text.strip().split():
+                output.add(item)
+        for headline2 in soup.find_all("h2"):
+            for item in headline2.text.strip().split():
+                output.add(item)
+        title = soup.find("meta",property="of:title")
+        if title:
+            output.add(title)
+        
+        for links in soup.find_all(href=True):
+            #print(links['href'])
+            if deep>0:
+                deep = deep - 1
+                crawl_miner(links['href'],output,deep)
+
+output = set()
+url = 'https://github.com'
+deep = 2
+crawl = crawl_miner(url,output,deep)
+
+print(output)
