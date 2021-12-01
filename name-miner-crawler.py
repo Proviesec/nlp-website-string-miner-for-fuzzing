a590f2877c48fb82598d2df01c960fe82f3b31e5
Oliver Trabhardt <oliver.trabhardt@googlemail.com>
2021-12-01 17:25:23 +0100

    Update name-miner-crawler.py

--- name-miner-crawler.py
+++ name-miner-crawler.py
@@ -2,3 +2,11 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
+r = requests.get('https://github.com')
+
+if r.status_code == 200:
+	soup: bs4.BeautifulSoup = BeautifulSoup(r.content, "html.parser")
+    
+    for headline in soup.find_all("span", {"class": "mw-headline"}):
+        print(headline.text)
+        
